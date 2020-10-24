dc33664f8178a6e22fece9a327d33d58786eabf8
Romain Tello <romain.tello@gmail.com>
2020-10-24 22:59:02 +0200

    Add comments in AbstractModel

--- App/Core/AbstractModel.php
+++ App/Core/AbstractModel.php
@@ -2,31 +2,75 @@
 // ================================================================
 // Espace de nommage (namespace)
 // ----------------------------------------------------------------
-// ~ 
+// ~ Les espaces de nommage permettent d'encapsuler des classes et
+// des méthodes afin d'éviter les collisions de nom. Par exemple,
+// je peux créer une nouvelle classe DateTime dans l'espace de
+// nommage App\Core, ce qui me permettra de la distinguer de la
+// clase native \DateTime.
+// ~ La norme PSR-4 prévoit que chaque classe doit être écrite dans
+// un fichir portant son nom, et que l'espace de nommage doit
+// correspondre à la hiérarchie de dossiers dans lequel ce fichier
+// se trouve. Par exemple, dans le fichier App\Core\DateTime.php,
+// je dois trouver la classe DateTime à l'intérieur de l'espace
+// de nommage App\Core.
+// ~ Composer permet de mettre en place un chargement automatique
+// (autoload) des fichiers s'ils sont organisés selon la norme
+// PSR-4.
 // ================================================================
 
 namespace App\Core;
 
 // ================================================================
-// 
+// Dépendances
 // ----------------------------------------------------------------
-// ~ 
+// ~ Le mot clé "use" permet de spécifier un nom de classe avec
+// son espace de nommage, afin qu'il soit chargé automatiquement
+// par Composer.
+// ~ Chaque classe doit déclarer avec "use" l'ensemble des classes
+// qu'elle utilise dans son propre code.
 // ================================================================
 
 use App\Core\DatabaseHandler;
 
 // ================================================================
-// 
+// Classe
 // ----------------------------------------------------------------
-// ~ 
+// ~ Une classe est un plan de construction permettant de générer
+// des objets (ou instances) conformes à un schéma. Ainsi, on sait
+// que tous les objets d'une même classe partagent un même ensemble
+// de propriétés et de comportements.
+// ~ Une classe est distincte des objets qu'elle génère. Les
+// propriétés et méthodes qu'elle définit appartient à ses
+// instances. Les propriétés et méthodes statiques appartiennent
+// à la classe.
+// ~ Une classe abstraite ne peut pas générer d'objets directement.
+// C'est une classe "incomplète" qui permet de factoriser des
+// éléments communs entre plusieurs classes, mais elle doit
+// obligatoirement être "complétée" par une classe-enfant pour
+// être fonctionnelle. En l'occurrence, la classe AbstractModel
+// contient toute la logique permettant de gérer l'accès à la base
+// de données, les modèles concrets (qui correspondent à une table
+// dans la base de données) n'ont plus qu'à la réutiliser.
 // ================================================================
 
 abstract class AbstractModel
 {
     // ================================================================
-    // PROPRIETES
+    // Propriétés
     // ----------------------------------------------------------------
-    // - Propriétés des instances de cette classe
+    // ~ Chaque classe définit un ensemble de propriétés pour toutes
+    // ses instances sous la forme de variables.
+    // ~ Chaque propriété peut être:
+    //  *publique (public): tous les processus peuvent librement
+    //   accéder au contenu de la propriété et le modifier.
+    //  *privée (private): seuls les processus affiliés à cette classe
+    //   peuvent accéder au contenu de la propriété et le modifier.
+    //  *protégée (protected): seuls les processus affiliés à cette
+    //   classe, ou à ses parents, ou à ses enfants, peuvent accéder
+    //   au contenu de la propriété et la modifier.
+    // ~ On préfère mettre toutes les propriétés en mode "protected"
+    // (ou "private") afin de prévenir les mauvaises utilisations de
+    // notre code.
     // ================================================================
 
     /**
@@ -35,9 +79,29 @@ abstract class AbstractModel
     protected $id;
 
     // ================================================================
-    // CONSTRUCTEUR
+    // Méthodes
     // ----------------------------------------------------------------
-    // - Permet de créer une nouvelle instance de cette clase
+    // ~ Une méthode est une fonction définie dans une classe, qui
+    // définira donc un comportement partagé par toutes ses instances.
+    // Par exemple, la classe \DateTime contient une méthode format(),
+    // qui permet à chaque date de s'afficher d'une manière précise.
+    // ~ Depuis PHP 7, on peut rajouter un "type-hint" (indication de
+    // type) aux paramètres et à la valeur de retour de chaque méthode.
+    // Si on passe une valeur n'ayant pas le type spécifié, PHP tentera
+    // d'abord de convertir la valeur donnée dans le type spécifié,
+    // puis générera une erreur s'il échoue.
+    // ================================================================
+
+    // ================================================================
+    // Constructeur
+    // ----------------------------------------------------------------
+    // ~ Méthode "magique" (c'est-à-dire dont le nom est réservé par
+    // le langage) qui s'exécute automatiquement lorsqu'une instance
+    // de la classe est créée. Elle permet classiquement de passer
+    // un ensemble de valeurs permettant d'initialiser les propriétés
+    // de l'instance.
+    // ~ Comme les propriétés, les méthodes peuvent être publiques,
+    // privées ou protégées.
     // ================================================================
 
     /**
@@ -51,10 +115,18 @@ abstract class AbstractModel
     }
 
     // ================================================================
-    // GETTERS & SETTERS
+    // Getters & Setters
     // ----------------------------------------------------------------
-    // - Permettent d'accéder aux propriétés des instances de cette
-    // classe
+    // ~ Méthodes permettant de contrôler l'accès aux propriétés des
+    // instances de la classe. On met toutes les propriétés en
+    // "protected" (ou "private") afin d'empêcher l'accès libre, et
+    // de forcer l'utilisation de "gettere" (de "get" = obtenir) pour
+    // lire les propriétés et de "setters" (de "set" = définir) pour
+    // les modifier.
+    // ~ On peut donc inclure la logique que l'on souhaite, comme par
+    // exemple une étape de validation avant d'enregistrer la nouvelle
+    // valeur souhaitée, ou encore avoir des propriétés en lecture
+    // seule (avec seulement un "getter" et pas de "setter"). 
     // ================================================================
 
     /**
@@ -68,9 +140,16 @@ abstract class AbstractModel
     }
 
     // ================================================================
-    // 
+    // Relations
     // ----------------------------------------------------------------
-    // ~ 
+    // ~ Ces méthodes permettent de gérer les relations entre des
+    // couples de modèles. Par exemple, si un article peut appartenir
+    // à une et une seule catégorie (Many-To-One), et qu'une catégorie
+    // peut contenir n'importe quel nombre d'articles (One-To-Many),
+    // la méthode getManyToOne(Category::class, 'category_id')
+    // permettra à un Article d'accéder à sa catégorie, alors que la
+    // méthode getOneToMany(Article::class, 'category_id') permettra à
+    // une Category de récupérer la liste de ses articles.
     // ================================================================
     
     /**
@@ -112,9 +191,14 @@ abstract class AbstractModel
     }
 
     // ================================================================
-    // 
+    // Accès à la base de données
     // ----------------------------------------------------------------
-    // ~ 
+    // ~ Ces méthodes permettent aux modèles d'interagir avec la base
+    // de données, aussi bien pour des requêtes (find*) que pour des
+    // mutations (insert, update, delete). Ces méthodes sont destinées
+    // à être appelées par les modèles concrets, qui devront donc leur
+    // passer le nom de la table concernée (ainsi que la liste des
+    // colonnes, le cas échéant).
     // ================================================================
 
     /**
@@ -287,9 +371,17 @@ abstract class AbstractModel
     }
 
     // ================================================================
-    // 
+    // Méthodes abstraites
     // ----------------------------------------------------------------
-    // ~ 
+    // ~ Une méthode abstraite est une simple déclaration de méthode
+    // (visibilité, nom, paramètres, type de retour), sans contenu.
+    // Cela oblige les classes dérivant de celle-ci à implémenter
+    // l'ensemble de ces méthodes, car l'on sait qu'elles sont
+    // indispensables à leur bon fonctionnement. Mais comme ces
+    // méthodes n'ont pas de sens en-dehors d'un modèle concret
+    // (associé à une table en base de données), on ne les définit pas
+    // dans la classe abstraite; on se contente d'exiger sa présence
+    // dans chaque classe dérivée.
     // ================================================================
 
     /**
