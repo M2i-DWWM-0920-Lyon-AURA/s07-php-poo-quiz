2e8b198c69cdb52d78357a10d8ce94323ce8f981
Romain Tello <romain.tello@gmail.com>
2020-10-26 22:20:31 +0100

    Add comments

--- App/Controller/ErrorController.php
+++ App/Controller/ErrorController.php
@@ -6,8 +6,13 @@ use App\View\StandardView;
 
 class ErrorController
 {
+    /**
+     * Display a "page not found" page
+     */
     public function notFound()
     {
+        // Remplace le code de réponse par défaut (200: OK) par une code d'erreur (404: Not Found)
+        // afin de signifier au client que la requête s'est terminée en erreur
         \http_response_code(404);
 
         return new StandardView(
--- index.php
+++ index.php
@@ -1,28 +1,61 @@
 <?php
+// ================================================================
+// Front controller
+// ----------------------------------------------------------------
+// ~ Toutes les requêtes au serveur sont redirigées sur ce fichier
+// grâce au fichier .htaccess.
+// ~ Le Front controller a pour mission d'intercepter toutes les
+// requêtes et d'envoyer l'utilisateur à l'endroit adéquat de
+// l'application.
+// ================================================================
+
+// Utilise le chargement automatique de Composer
+require_once './vendor/autoload.php';
 
 use App\View\StandardView;
 use App\Controller\ErrorController;
 use App\Exception\RecordNotFoundException;
 
-// Utilise le chargement automatique de Composer
-require_once './vendor/autoload.php';
+// ================================================================
+// Gestion des erreurs
+// ----------------------------------------------------------------
+// ~ On peut emballer une série d'instructions dans un bloc "try"
+// (essayer), et ajouter à la suite un bloc "catch" (attraper). Si
+// une erreur est détectée dans le bloc "try", PHP passe aussitôt
+// dans le bloc "catch" au lieu de s'arrêter à l'erreur.
+// ~ Ceci nous permet de renvoyer l'utilisateur, par exemple à une
+// page d'erreur 404, en générant des erreurs personnalisées aux
+// endroits adéquats de notre code.
+// ================================================================
 
 // Essaie d'exécuter le programme normalement
 try {
+    // ================================================================
+    // Routeur
+    // ----------------------------------------------------------------
+    // ~ Chaque requête contient une méthode (GET, POST...) et une URL.
+    // Chaque combinaison de méthode/URL prévue dans une application
+    // s'appelle une "route". Le routeur est un composant chargé de
+    // trouver une correspondance entre la requête de l'utilisateur et
+    // l'une des routes de l'application.
+    // ~ On fait correspondre à chaque route un nom de contrôleur et
+    // une méthode. Ainsi, on a la liberté de faire correspondre
+    // n'importe quel comportement à chaque route.
+    // ================================================================
+
     // Crée une nouvelle instance du routeur
     $router = new AltoRouter();
 
-    // Génère la route pour la page d'accueil
+    // Configure le routeur avec toutes les routes de l'application
     $router->map('GET', '/', 'MainController#home');
-    // Génère la route pour la liste des quiz
     $router->map('GET', '/quiz', 'QuizController#list');
-    // Génère la route pour la liste des quiz
     $router->map('GET', '/quiz/[i:id]', 'QuizController#single');
     // Prend la requête actuelle et cherche une correspondance avec les routes connues
     $match = $router->match();
 
     // Si la requête ne correspond à aucune route connue
     if ($match === false) {
+        // Renvoie une page "Page non trouvée"
         $controller = new ErrorController;
         $response = $controller->notFound();
     } else {
@@ -40,10 +73,12 @@ try {
 }
 // Si une erreur liée à un enregistrement non existant en BDD est détectée
 catch (RecordNotFoundException $exception) {
+    // Renvoie une page "Page non trouvée"
     $controller = new ErrorController;
     $response = $controller->notFound();
 }
 
 // Une fois que le contrôleur approprié a été appelé, affiche le résultat
 $response->render();
+// Interrompt le programme
 die();
