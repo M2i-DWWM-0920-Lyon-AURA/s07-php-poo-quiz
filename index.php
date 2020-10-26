595d4421fbc0f9a434e3fc55e65be8867229d580
Romain Tello <romain.tello@gmail.com>
2020-10-26 12:33:28 +0100

    Add basic routing with pages

--- index.php
+++ index.php
@@ -2,3 +2,20 @@
 
 // Utilise le chargement automatique de Composer
 require_once './vendor/autoload.php';
+
+// Crée une nouvelle instance du routeur
+$router = new AltoRouter();
+
+// Génère la route pour la page d'accueil
+$router->map('GET', '/', 'home.php');
+// Génère la route pour la liset des quiz
+$router->map('GET', '/quiz', 'quiz-list.php');
+// Prend la requête actuelle et cherche une correspondance avec les routes connues
+$match = $router->match();
+
+// Si la requête ne correspond à aucune route connue
+if ($match === false) {
+    include './templates/page-not-found.php';
+} else {
+    include './templates/' . $match['target'];
+}
---
+++ templates/page-not-found.php
@@ -0,0 +1,14 @@
+<!-- Page not found template -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <img src="" />
+    <h1>Page non trouvée</h1>
+    <p>Malheureusement, malgré tous nos efforts, nous n'avons pas réussi à trouver une page correspondant à votre requête... 😥</p>
+</body>
+</html>
